2ea3fa89815df33a5fe51fdf060afca237bbaae7
Mushtaq <mushtaqat3gb@gmail.com>
2020-06-14 16:46:33 +0530

    Add project files.

---
+++ LocalizationSample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalizationSample
+{
+    public partial class App : Application
+    {
+        public static int SelectedIndex = -1;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ LocalizationSample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using LocalizationSample.Resx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalizationSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            string language = Thread.CurrentThread.CurrentUICulture.Name;
+            picker.SelectedIndex = language == "ta" ? 2 : language == "fr" ? 1 : 0;
+        }
+
+        private void picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (App.SelectedIndex == picker.SelectedIndex)
+                return;
+            App.SelectedIndex = picker.SelectedIndex;
+            CultureInfo language = new CultureInfo(picker.SelectedIndex == 0 ? "" : picker.SelectedIndex == 1 ? "fr" : "ta");
+            Thread.CurrentThread.CurrentUICulture = language;
+            AppResources.Culture = language;
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
